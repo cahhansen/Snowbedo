932f8df977865b94a6251a2b0a701054f7f47b4c
cahhansen <carlyahansen@gmail.com>
2017-03-02 17:59:16 -0700

    Improve temperature sensitivity

--- R/modelflow.R
+++ R/modelflow.R
@@ -4,11 +4,15 @@
 #' @param meltcoefficient (melt factor*snow runoff coefficient*radiation coefficient)
 #' @param runoffcoefficient (between 0-1)
 #' @param area Area of watershed in Km2
+#' @param baseflow Discharge in cms
 #'
 #' @return vector of modeled flow
 
-modelflow=function(data,meltcoefficient,runoffcoefficient,area){
-  flow=(((meltcoefficient*data$solar_short_Whm2day)*(1-data$albedo)*
-      (data$tavg-0))*data$snowcover)+(runoffcoefficient*data$precip_daily)*area*(10000/86400)
-  return(flow)
+modelflow=function(data,meltcoefficient,runoffcoefficient,area,baseflow){
+  snowmelt=ifelse((data$tavg>0),((meltcoefficient*data$solar_short_Whm2day)*(1-data$albedo)*(data$tavg-0)),0)
+  runoff = ifelse((data$tavg>0),runoffcoefficient*data$precip_daily,0)
+  discharge=((snowmelt*data$snowcover)+runoff)*area*(10000/86400)+baseflow
+  return(discharge)
 }
+
+
