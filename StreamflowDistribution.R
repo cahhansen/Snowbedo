633275e836a588dd6ee003547308ca9e93fec2f9
cahhansen <carlyahansen@gmail.com>
2018-01-17 21:53:28 -0700

    add script for calculating streamflow distribution stats

---
+++ StreamflowDistribution.R
@@ -0,0 +1,12 @@
+#Read in daily streamflow dataset
+
+#Format if needed
+
+#Add month and year column
+
+#Find distribution characteristics (mean, st.dev, and correlation) by month
+
+#Find max and min year
+
+
+#Print Results
